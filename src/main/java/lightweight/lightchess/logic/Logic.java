68eb975bccd5b90a8e84db0bf8aa0359f034ba3c
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2022-02-23 18:51:41 +0600

    makeMove checks sanity and then moves

--- src/main/java/lightweight/lightchess/logic/Logic.java
+++ src/main/java/lightweight/lightchess/logic/Logic.java
@@ -24,7 +24,9 @@ public class Logic {
     }
 
     public static boolean makeMove(Board board, Move move) {
-        if(board.doMove(move)) {
+
+        if(isLegal(board, move)) {
+            board.doMove(move);
             prevFrom = move.getFrom().toString().toLowerCase();
             prevTo = move.getTo().toString().toLowerCase();
             return true;
@@ -33,7 +35,8 @@ public class Logic {
     }
 
     public static boolean makeMove(Board board, String move) {
-        if(board.doMove(move)) {
+        if(isLegal(board, move)) {
+            board.doMove(move);
             prevFrom = move.substring(0,2);
             prevTo = move.substring(2,4);
             return true;
@@ -45,6 +48,16 @@ public class Logic {
         List<Move> legalMoves = b.legalMoves();
         for(Move move : legalMoves) {
             if(move.toString().equals(m.toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isLegal(Board b, String m) {
+        List<Move> legalMoves = b.legalMoves();
+        for(Move move : legalMoves) {
+            if(move.toString().equals(m)) {
                 return true;
             }
         }
@@ -65,11 +78,7 @@ public class Logic {
 
     public static void main(String[] args) {
         Board b = new Board();
-//        String FEN = "rnbqkbnr/1ppppQp1/7p/p7/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4";
-//        b.loadFromFen(FEN);
-        makeMove(b, "e2e4");
-//        makeMove(b, new Move(Square.E1, Square.E2));
+        makeMove(b, "d1d4");
         System.out.println(b.toString());
-        System.out.println(">>>" + prevFrom + prevTo);
     }
 }
