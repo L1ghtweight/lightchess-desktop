0a5d2962334993567f4400ff5b13d70a02fe2ddb
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2022-02-23 16:47:41 +0600

    progress on chess logic

--- src/main/java/lightweight/lightchess/logic/Logic.java
+++ src/main/java/lightweight/lightchess/logic/Logic.java
@@ -11,6 +11,7 @@ import java.util.Locale;
 
 
 public class Logic {
+    public static String prevFrom = new String(), prevTo = new String();
     public static List<Move> getLegalMovesFromSquare(Board b, String pos) {
         List<Move> allLegalMoves = b.legalMoves();
         List<Move> legalFromSquare = new ArrayList<>();
@@ -20,6 +21,24 @@ public class Logic {
             }
         }
         return legalFromSquare;
+    }
+
+    public static boolean makeMove(Board board, Move move) {
+        if(board.doMove(move)) {
+            prevFrom = move.getFrom().toString().toLowerCase();
+            prevTo = move.getTo().toString().toLowerCase();
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean makeMove(Board board, String move) {
+        if(board.doMove(move)) {
+            prevFrom = move.substring(0,2);
+            prevTo = move.substring(2,4);
+            return true;
+        }
+        return false;
     }
 
     public static boolean isLegal(Board b, Move m) {
@@ -32,10 +51,25 @@ public class Logic {
         return false;
     }
 
+    public static boolean isCheckmate(Board board) {
+        return board.isMated();
+    }
+
+    public static boolean isCheck(Board board) {
+        return board.isKingAttacked();
+    }
+
+//    public static Side getSideToMove(Board board) {
+//        return board.getSideToMove();
+//    }
+
     public static void main(String[] args) {
         Board b = new Board();
-        String FEN = "rnbqkbnr/1ppppQp1/7p/p7/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4";
-        b.loadFromFen(FEN);
+//        String FEN = "rnbqkbnr/1ppppQp1/7p/p7/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4";
+//        b.loadFromFen(FEN);
+        makeMove(b, "e2e4");
+//        makeMove(b, new Move(Square.E1, Square.E2));
         System.out.println(b.toString());
+        System.out.println(">>>" + prevFrom + prevTo);
     }
 }
